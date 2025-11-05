186eb57afa43a9d969d57c9d716cc0665fdf417b
ilyesfahd <ilyas45miloudi@gmail.com>
2025-11-05 14:15:00 +0100

    Ajout requetes BDD

--- modele/bd.inc.php
+++ modele/bd.inc.php
@@ -1,20 +1,19 @@
 <?php
 
-session_start();
-
-function connexionPDO() {
+function connexionPDO()
+{
     $login = 'root';
     $mdp = '';
-    $bd = 'gsbcr0';
+    $bd = 'test';
     $serveur = 'localhost';
 
     try {
-        $conn = new PDO("mysql:host=$serveur;port=3307;dbname=$bd",$login,$mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')); 
+        $conn = new PDO("mysql:host=$serveur;port=3307;dbname=$bd", $login, $mdp, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $conn;
     } catch (PDOException $e) {
-        print("Erreur de connexion PDO :");
-		print($e);
+        print ("Erreur de connexion PDO :");
+        print ($e);
         die();
     }
 }
